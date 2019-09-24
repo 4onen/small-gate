243ac86854ece5278639f40215e2b414f0740bf1
Matthew Dupree <mattjdupree@gmail.com>
2019-09-24 12:45:12 -0700

    Add Capacitance module and function to calculate input capacitance per input.

---
+++ src/GateSchematic/Capacitance.elm
@@ -0,0 +1,25 @@
+module Capacitance exposing (input)
+
+import Dict exposing (Dict)
+import GateSchematic.Types exposing (Input, Width)
+import Strand exposing (Alignment(..))
+
+
+input : Alignment Input -> Dict String Width
+input =
+    let
+        sum_dicts =
+            List.foldl
+                (Dict.merge
+                    Dict.insert
+                    (\key a b -> Dict.insert key (a + b))
+                    Dict.insert
+                    Dict.empty
+                )
+                Dict.empty
+    in
+    Strand.fold
+        { single = \( name, ( nmos, pmos ) ) -> Dict.singleton name (nmos + pmos)
+        , strand = sum_dicts
+        , fray = sum_dicts
+        }
