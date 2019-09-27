dcf15e98014c8801d1fd05c659b7ae9440cd1637
Matthew Dupree <mattjdupree@gmail.com>
2019-09-27 10:04:52 -0700

    Complete diode-model Elmore rise time calc.

--- src/GateSchematic/Delay.elm
+++ src/GateSchematic/Delay.elm
@@ -1,7 +1,7 @@
 module GateSchematic.Delay exposing (..)
 
 import GateSchematic.Logic
-import GateSchematic.Types exposing (Transistor, TransistorKind(..))
+import GateSchematic.Types exposing (Transistor, TransistorKind(..), Width)
 import List.Extra
 import Set exposing (Set)
 import Strand exposing (Alignment(..))
@@ -22,13 +22,20 @@ computeRiseFall gate outputCapacitance =
     ( rise, rise )
 
 
+type alias Path =
+    { currentCarrying : Maybe Float
+    , delay : Float -> Float
+    , outputCapacitance : Float
+    }
+
+
 computeRise : Alignment ( String, Float ) -> Alignment ( String, Float ) -> Float -> List ( Set String, Float )
 computeRise pmos nmos outputCapacitance =
     let
         lactives =
             GateSchematic.Logic.activeSets (Strand.map Tuple.first pmos)
 
-        transistor transistorKind width =
+        transistor active transistorKind width =
             let
                 resistanceFactor =
                     case transistorKind of
@@ -39,92 +46,92 @@ computeRise pmos nmos outputCapacitance =
                             1.0
 
                 resistance =
-                    resistanceFactor / width
-            in
-            { resistance = resistance
-            , delay =
-                \beforeResistance otherPathsAfter ->
-                    case otherPathsAfter of
-                        Just otherPath ->
-                            beforeResistance * width + otherPath * width
+                    if active then
+                        Just <| resistanceFactor / width
 
-                        Nothing ->
-                            beforeResistance * width + (beforeResistance + resistance) * width
-            }
+                    else
+                        Nothing
+            in
+            Path resistance (\beforeResistance -> beforeResistance * width) width
 
         wire =
-            { resistance = 0, delay = always always 0 }
+            Path (Just 0) (always 0) 0
 
         open =
-            { resistance = 1 / 0, delay = always always (1 / 0) }
+            Path Nothing (always 0) 0
     in
-    List.map
+    List.filterMap
         (\active ->
             let
+                pmosSide : Path
                 pmosSide =
-                    pmos
-                        |> Strand.fold
-                            { single =
-                                \( name, width ) ->
-                                    if Set.member name active then
-                                        Just <| transistor PMOS width
+                    Strand.fold
+                        { single =
+                            \( name, width ) ->
+                                transistor (Set.member name active) PMOS width
+                        , strand =
+                            List.foldl
+                                (\new total ->
+                                    case total.currentCarrying of
+                                        Nothing ->
+                                            Path Nothing total.delay new.outputCapacitance
 
-                                    else
-                                        Nothing
-                            , strand =
-                                List.foldl
-                                    (Maybe.map2
-                                        (\new total ->
-                                            { resistance = total.resistance + new.resistance
-                                            , delay =
-                                                \beforeResistance otherPathsAfter ->
-                                                    total.delay beforeResistance Nothing + new.delay (total.resistance + beforeResistance) otherPathsAfter
-                                            }
-                                        )
-                                    )
-                                    (Just wire)
-                            , fray =
-                                List.foldl
-                                    (\mnew mtotal ->
-                                        case ( mnew, mtotal ) of
-                                            ( Just new, Nothing ) ->
-                                                mnew
+                                        Just currentResistance ->
+                                            Path
+                                                (Maybe.map ((+) currentResistance) new.currentCarrying)
+                                                (\beforeResistance ->
+                                                    (+)
+                                                        ((+) (total.delay beforeResistance) (total.outputCapacitance * (beforeResistance + currentResistance)))
+                                                        (new.delay <| beforeResistance + currentResistance)
+                                                )
+                                                new.outputCapacitance
+                                )
+                                wire
+                        , fray =
+                            List.foldl
+                                (\new total ->
+                                    case total.currentCarrying of
+                                        Nothing ->
+                                            Path new.currentCarrying
+                                                (\beforeResistance ->
+                                                    (+)
+                                                        (total.delay beforeResistance)
+                                                        (new.delay beforeResistance)
+                                                )
+                                                (new.outputCapacitance + total.outputCapacitance)
 
-                                            ( Nothing, Just total ) ->
-                                                mtotal
-
-                                            ( Nothing, Nothing ) ->
-                                                Nothing
-
-                                            ( Just new, Just total ) ->
-                                                Just
-                                                    { resistance = parallel new.resistance total.resistance
-                                                    , delay =
-                                                        \beforeResistance _ ->
-                                                            Basics.min
-                                                                (new.delay beforeResistance Nothing)
-                                                                (total.delay beforeResistance Nothing)
-                                                    }
-                                    )
-                                    Nothing
-                                    >> Maybe.map
-                                        (\({ resistance, delay } as paths) ->
-                                            { paths
-                                                | delay =
-                                                    \beforeResistance _ ->
-                                                        paths.delay beforeResistance (Just <| beforeResistance + paths.resistance)
-                                            }
-                                        )
-                            }
-                        |> Maybe.withDefault open
-
-                parasiticDelay =
-                    pmosSide.delay 0 Nothing
-
-                effortDelay =
-                    pmosSide.resistance * outputCapacitance
+                                        Just currentResistance ->
+                                            let
+                                                newResistance =
+                                                    new.currentCarrying
+                                                        |> Maybe.map (parallel currentResistance)
+                                                        |> Maybe.withDefault currentResistance
+                                            in
+                                            Path
+                                                (Just newResistance)
+                                                (\before ->
+                                                    (+)
+                                                        (total.delay before)
+                                                        (new.delay before)
+                                                )
+                                                (total.outputCapacitance + new.outputCapacitance)
+                                )
+                                open
+                        }
+                        pmos
             in
-            ( active, parasiticDelay + effortDelay )
+            pmosSide.currentCarrying
+                |> Maybe.map
+                    (\r ->
+                        let
+                            parasiticDelay =
+                                r * pmosSide.outputCapacitance + pmosSide.delay 0
+
+                            effortDelay =
+                                r * outputCapacitance
+                        in
+                        ( active, parasiticDelay + effortDelay )
+                    )
         )
         lactives
 
