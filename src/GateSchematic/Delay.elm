3524938a78e05264951d0f7ca3248382de4a0c50
Matthew Dupree <mattjdupree@gmail.com>
2019-09-30 16:52:38 -0700

    Reduce copied code in rise/fall delay calculations.

--- src/GateSchematic/Delay.elm
+++ src/GateSchematic/Delay.elm
@@ -36,108 +36,17 @@ computeRise pmos nmos outputCapacitance =
     let
         lactives =
             GateSchematic.Logic.activeSets (Strand.map Tuple.first pmos)
-
-        transistor active transistorKind width =
-            let
-                resistanceFactor =
-                    case transistorKind of
-                        PMOS ->
-                            2.0
-
-                        NMOS ->
-                            1.0
-
-                currentCarrying =
-                    if active then
-                        Just <| resistanceFactor / width
-
-                    else
-                        Nothing
-
-                delay mbefore mafter =
-                    case ( mbefore, mafter ) of
-                        ( Nothing, Nothing ) ->
-                            0
-
-                        ( Just rbefore, Nothing ) ->
-                            case currentCarrying of
-                                Just r ->
-                                    (rbefore * width) + ((rbefore + r) * width)
-
-                                Nothing ->
-                                    rbefore * width
-
-                        ( Nothing, Just rafter ) ->
-                            case currentCarrying of
-                                Just r ->
-                                    ((rafter + r) * width) + (rafter * width)
-
-                                Nothing ->
-                                    rafter * width
-
-                        ( Just rbefore, Just rafter ) ->
-                            case currentCarrying of
-                                Just r ->
-                                    (+)
-                                        (width * parallel rbefore (rafter + r))
-                                        (width * parallel (rbefore + r) rafter)
-
-                                Nothing ->
-                                    (+)
-                                        (width * rbefore)
-                                        (width * rafter)
-            in
-            CurrentPath currentCarrying delay
-
-        wire =
-            CurrentPath (Just 0) (always (always 0))
-
-        open =
-            CurrentPath Nothing (always (always 0))
     in
     List.filterMap
         (\active ->
             let
                 pmosSide : CurrentPath
                 pmosSide =
-                    Strand.fold
-                        { single =
-                            \( name, width ) ->
-                                transistor (Set.member name active) PMOS width
-                        , strand =
-                            List.foldl
-                                (\new total ->
-                                    CurrentPath
-                                        (Maybe.map2 (+) total.currentCarrying new.currentCarrying)
-                                        (\mbefore mafter ->
-                                            (+)
-                                                (total.delay mbefore (Maybe.map2 (+) new.currentCarrying mafter))
-                                                (new.delay (Maybe.map2 (+) total.currentCarrying mbefore) mafter)
-                                        )
-                                )
-                                wire
-                        , fray =
-                            \lp ->
-                                List.Extra.indexedFoldl
-                                    (\i new total ->
-                                        CurrentPath
-                                            (maybeParallel new.currentCarrying total.currentCarrying)
-                                            (let
-                                                otherPaths =
-                                                    lp
-                                                        |> List.Extra.removeAt i
-                                                        |> List.foldl (.currentCarrying >> maybeParallel) Nothing
-                                             in
-                                             \mbefore mafter ->
-                                                (+)
-                                                    (total.delay mbefore mafter)
-                                                    (new.delay mbefore (maybeParallel otherPaths mafter))
-                                            )
-                                    )
-                                    open
-                                    lp
-                        }
-                        pmos
+                    currentPath PMOS False active pmos
+
+                nmosSide : CurrentPath
+                nmosSide =
+                    currentPath NMOS False active nmos
 
                 parasitic =
                     pmosSide.delay (Just 0) Nothing
@@ -153,111 +62,20 @@ computeRise pmos nmos outputCapacitance =
 computeFall : Alignment ( String, Float ) -> Alignment ( String, Float ) -> Float -> List ( Set String, Float )
 computeFall pmos nmos outputCapacitance =
     let
+        lactives : List (Set String)
         lactives =
             GateSchematic.Logic.activeSets (Strand.map Tuple.first nmos)
-
-        transistor active transistorKind width =
-            let
-                resistanceFactor =
-                    case transistorKind of
-                        PMOS ->
-                            2.0
-
-                        NMOS ->
-                            1.0
-
-                currentCarrying =
-                    if active then
-                        Just <| resistanceFactor / width
-
-                    else
-                        Nothing
-
-                delay mbefore mafter =
-                    case ( mbefore, mafter ) of
-                        ( Nothing, Nothing ) ->
-                            0
-
-                        ( Just rbefore, Nothing ) ->
-                            case currentCarrying of
-                                Just r ->
-                                    (rbefore * width) + ((rbefore + r) * width)
-
-                                Nothing ->
-                                    rbefore * width
-
-                        ( Nothing, Just rafter ) ->
-                            case currentCarrying of
-                                Just r ->
-                                    ((rafter + r) * width) + (rafter * width)
-
-                                Nothing ->
-                                    rafter * width
-
-                        ( Just rbefore, Just rafter ) ->
-                            case currentCarrying of
-                                Just r ->
-                                    (+)
-                                        (width * parallel rbefore (rafter + r))
-                                        (width * parallel (rbefore + r) rafter)
-
-                                Nothing ->
-                                    (+)
-                                        (width * rbefore)
-                                        (width * rafter)
-            in
-            CurrentPath currentCarrying delay
-
-        wire =
-            CurrentPath (Just 0) (always (always 0))
-
-        open =
-            CurrentPath Nothing (always (always 0))
     in
     List.filterMap
         (\active ->
             let
+                pmosSide : CurrentPath
+                pmosSide =
+                    currentPath PMOS True active pmos
+
                 nmosSide : CurrentPath
                 nmosSide =
-                    Strand.fold
-                        { single =
-                            \( name, width ) ->
-                                transistor (Set.member name active) NMOS width
-                        , strand =
-                            List.reverse
-                                >> List.foldl
-                                    (\new total ->
-                                        CurrentPath
-                                            (Maybe.map2 (+) total.currentCarrying new.currentCarrying)
-                                            (\mbefore mafter ->
-                                                (+)
-                                                    (total.delay mbefore (Maybe.map2 (+) new.currentCarrying mafter))
-                                                    (new.delay (Maybe.map2 (+) total.currentCarrying mbefore) mafter)
-                                            )
-                                    )
-                                    wire
-                        , fray =
-                            \lp ->
-                                List.Extra.indexedFoldl
-                                    (\i new total ->
-                                        CurrentPath
-                                            (maybeParallel new.currentCarrying total.currentCarrying)
-                                            (let
-                                                otherPaths =
-                                                    lp
-                                                        |> List.Extra.removeAt i
-                                                        |> List.foldl (.currentCarrying >> maybeParallel) Nothing
-                                             in
-                                             \mbefore mafter ->
-                                                (+)
-                                                    (total.delay mbefore mafter)
-                                                    (new.delay mbefore (maybeParallel otherPaths mafter))
-                                            )
-                                    )
-                                    open
-                                    lp
-                        }
-                        nmos
+                    currentPath NMOS True active nmos
 
                 parasitic =
                     nmosSide.delay (Just 0) Nothing
@@ -268,6 +86,115 @@ computeFall pmos nmos outputCapacitance =
             Maybe.map ((+) parasitic >> Tuple.pair active) load
         )
         lactives
+
+
+currentPath : TransistorKind -> Bool -> Set String -> Alignment ( String, Float ) -> CurrentPath
+currentPath tkind reverse active =
+    Strand.fold
+        { single =
+            \( name, width ) ->
+                transistor (Set.member name active) tkind width
+        , strand =
+            (if reverse then
+                List.reverse
+
+             else
+                identity
+            )
+                >> List.foldl
+                    (\new total ->
+                        CurrentPath
+                            (Maybe.map2 (+) total.currentCarrying new.currentCarrying)
+                            (\mbefore mafter ->
+                                (+)
+                                    (total.delay mbefore (Maybe.map2 (+) new.currentCarrying mafter))
+                                    (new.delay (Maybe.map2 (+) total.currentCarrying mbefore) mafter)
+                            )
+                    )
+                    wire
+        , fray =
+            \lp ->
+                List.Extra.indexedFoldl
+                    (\i new total ->
+                        CurrentPath
+                            (maybeParallel new.currentCarrying total.currentCarrying)
+                            (let
+                                otherPaths =
+                                    lp
+                                        |> List.Extra.removeAt i
+                                        |> List.foldl (.currentCarrying >> maybeParallel) Nothing
+                             in
+                             \mbefore mafter ->
+                                (+)
+                                    (total.delay mbefore mafter)
+                                    (new.delay mbefore (maybeParallel otherPaths mafter))
+                            )
+                    )
+                    open
+                    lp
+        }
+
+
+wire =
+    CurrentPath (Just 0) (always (always 0))
+
+
+open =
+    CurrentPath Nothing (always (always 0))
+
+
+transistor : Bool -> TransistorKind -> Float -> CurrentPath
+transistor active transistorKind width =
+    let
+        resistanceFactor =
+            case transistorKind of
+                PMOS ->
+                    2.0
+
+                NMOS ->
+                    1.0
+
+        currentCarrying =
+            if active then
+                Just <| resistanceFactor / width
+
+            else
+                Nothing
+
+        delay mbefore mafter =
+            case ( mbefore, mafter ) of
+                ( Nothing, Nothing ) ->
+                    0
+
+                ( Just rbefore, Nothing ) ->
+                    case currentCarrying of
+                        Just r ->
+                            (rbefore * width) + ((rbefore + r) * width)
+
+                        Nothing ->
+                            rbefore * width
+
+                ( Nothing, Just rafter ) ->
+                    case currentCarrying of
+                        Just r ->
+                            ((rafter + r) * width) + (rafter * width)
+
+                        Nothing ->
+                            rafter * width
+
+                ( Just rbefore, Just rafter ) ->
+                    case currentCarrying of
+                        Just r ->
+                            (+)
+                                (width * parallel rbefore (rafter + r))
+                                (width * parallel (rbefore + r) rafter)
+
+                        Nothing ->
+                            (+)
+                                (width * rbefore)
+                                (width * rafter)
+    in
+    CurrentPath currentCarrying delay
 
 
 {-| The operation for combining resistors in parallel
